5c84ef733ecb010f4c68e9fa62774615ae59a27f
Ayushman KB <ayushmaan.banerjee@gmail.com>
2023-01-23 22:44:04 +0530

    Update mbin192.py
    * added backbone+sidechain
    * added ligand-neighbor within threshold radius
    * added amino-acid-neighbor within threshold radius

--- mbin192.py
+++ mbin192.py
@@ -10,6 +10,12 @@ from fastapi.templating import Jinja2Templates
 
 import Bio.PDB as pdb
 
+amino_acids = ["ala", "arg", "asn", "asp",
+               "cys", "glu", "gln", "gly",
+               "his", "ile", "leu", "lys",
+               "met", "phe", "pro", "ser",
+               "thr", "trp", "tyr", "val"]
+
 chi_atoms = dict(
         chi1=dict(
             ARG=['N', 'CA', 'CB', 'CG'],
@@ -82,13 +88,13 @@ def get_structure(id):
     parser = pdb.PDBParser(PERMISSIVE=True, QUIET=True)
     structures = parser.get_structure(id, "pdb"+id.lower()+".ent")
 
-    return structures[0]
+    return structures
 
 def getCNdistance(id):
 
     Ci_Ni_plus_1_bond_distances = []
 
-    for chain in get_structure(id):
+    for chain in get_structure(id)[0]:
         
         residues = chain.get_list()
         
@@ -154,7 +160,7 @@ def getPhiPsi(id):
 
     phi_psi_out = []
 
-    for chain in get_structure(id):
+    for chain in get_structure(id)[0]:
         # residues = chain.get_list()
         residues = pdb.CaPPBuilder().build_peptides(chain)
         for poly_index, poly in enumerate(residues):
@@ -185,7 +191,7 @@ def getChi1Chi2(id):
 
     chi1_chi2_out = []
 
-    for chain in get_structure(id):
+    for chain in get_structure(id)[0]:
         for residue in chain:
             # Skip heteroatoms
             if residue.id[0] != " ": continue
@@ -222,6 +228,141 @@ def getChi1Chi2plot(id, df):
     plt.title("Chi1/Chi2 lineplot for PDB:"+id.lower())
     plt.tight_layout()
     plt.savefig('static/'+str(id.lower())+'_chi.png', bbox_inches='tight')
+
+def get_backbone_sidechain(id):
+    
+    structures = get_structure(id)
+
+    chains = list(structures.get_chains())
+    if len(chains)%2==1:
+        chains = chains[:-1]
+
+    RESULT = []
+
+    for i in range(0, len(chains), 2):
+        strand1, strand2 = chains[i], chains[i+1]
+        s1=[]
+        s2=[]
+        for resx, resy in zip(strand1, strand2):
+            # Ignore water
+            if not resx.resname in ["HOH", "CA", "MG", "A23"] and not resy.resname in ["HOH", "CA", "MG", "A23"]:
+                s1.append(resx)
+                s2.append(resy)
+      
+
+        for i, j in zip(    range(0, len(s1)+1, 1)  ,   range(len(s2)-1, -1, -1)):
+            m = s1[i]
+            n = s2[j]
+            # For base-pair
+            
+            atom1 = {} 
+            atom2 = {}
+      
+            for atom in s1[i].get_list():
+                atom1[atom.get_fullname().replace(" ", "")] = atom.coord
+            for atom in s2[j].get_list():
+                atom2[atom.get_fullname().replace(" ", "")] = atom.coord
+
+            # Backbone
+            # O4-O4
+            if "O4'" in atom1.keys() and "O4'" in atom2.keys():
+                O4O4_dist = np.linalg.norm(atom1["O4'"] - atom2["O4'"])
+            else:
+                O4O4_dist = 0
+
+            # Sidechain
+            # N4-O6
+            if "N4" in atom1.keys() and "O6" in atom2.keys():
+                N4O6_dist = np.linalg.norm(atom1["N4"] - atom2["O6"])
+            elif "N4" in atom2.keys() and "O6" in atom1.keys():
+                N4O6_dist = np.linalg.norm(atom2["N4"] - atom1["O6"])
+            else:
+                N4O6_dist = 0
+
+            # N3-N1
+            if "N3" in atom1.keys() and "N1" in atom2.keys():
+                N3N1_dist1 = np.linalg.norm(atom1["N3"] - atom2["N1"])
+            if "N3" in atom2.keys() and "N1" in atom1.keys():
+                N3N1_dist2 = np.linalg.norm(atom2["N3"] - atom1["N1"])
+                N3N1_dist = min(N3N1_dist1, N3N1_dist2)
+            else:
+                N3N1_dist = 0
+
+            # O2-N2
+            if "O2" in atom1.keys() and "N2" in atom2.keys():
+                N2O2_dist = np.linalg.norm(atom1["O2"] - atom2["N2"])
+            elif "O2" in atom2.keys() and "N2" in atom1.keys():
+                N2O2_dist = np.linalg.norm(atom2["O2"] - atom1["N2"])
+            else:
+                N2O2_dist = 0
+            RESULT.append([strand1.id, strand2.id, m.resname+str(m.id[1]), n.resname+str(n.id[1]), O4O4_dist, N4O6_dist, N3N1_dist, N2O2_dist])
+    
+    RESULT = pd.DataFrame(RESULT, columns=['Chain F', 'Chain R', 'Nucleotide 1', 'Nucleotide 2', 'O4--O4', 'N4--O6', 'N3--N1', 'O2--N2'])
+    return RESULT
+
+
+def ligand_neighbors_distances(id, ligand, neighbor=3.5):
+    
+    ligand = ligand.upper()
+    structures = get_structure(id)
+
+    chains = list(structures.get_chains())
+    # Collect metal ion
+    metal=[]
+    for chain in chains:
+        for res in chain:
+            for atom in res:
+                if ligand in atom.id:
+                # print(atom.coord, atom.id, chain.id)
+                    metal.append([chain.id, atom.id, atom.coord, atom.full_id])
+
+    metal_neighbors = []
+    # Compare distances
+    for chain in chains:
+        for res in chain:
+            for atom in res:
+                if not ligand in atom.id:
+                    for m in metal:
+                        dist = np.linalg.norm(atom.coord - m[2])
+                        if dist < neighbor:
+                                metal_neighbors.append([id, m[0], str(m[1])+str(m[3][3][1]), chain.id, res.resname+str(res.id[1]), atom.id, dist])
+    metal_neighbors = pd.DataFrame(metal_neighbors, columns=['PDB_ID', 'Metal_Chain', 'Metal_Ion', 'Residue_Chain', 'Residue', 'Bonded_Atom', 'Distance'])
+    RESULT = metal_neighbors.loc[metal_neighbors.groupby(['Metal_Chain','Metal_Ion','Residue_Chain','Residue'])['Distance'].idxmin()]
+    return RESULT
+
+
+def aa_neighbors_distances(id, neighbor):
+    
+    structures = get_structure(id)
+    chains = list(structures.get_chains())
+    amino_acids = [a.upper() for a in amino_acids]
+
+    aa=[]
+    for chain in chains:
+      for res in chain:
+        if res.resname in amino_acids:
+          aa.append([chain.id, res.resname, res.get_list(), res.id[1]])
+
+    aa_neighbors=[]
+    for chain in chains:
+      for res in chain:
+        if not res in amino_acids+["HOH"]:
+          for atom in res:
+            for a in aa:
+              a_chain = a[0]
+              a_res = a[1]
+              for atom2 in a[2]:
+                dist = np.linalg.norm(atom.coord - atom2.coord)
+                if dist < neighbor:
+                  aa_neighbors.append([id, a_chain, a_res, str(a[3]), chain.id, res.resname, str(res.id[1])])
+
+    aa_neighbors = pd.DataFrame(aa_neighbors, columns=['PDB_ID', 'AA_Chain', 'Amino_Acid', 'AA_Position', 'Residue_Chain', 'Residue', 'Residue_Position'])
+    aa_neighbors = aa_neighbors[aa_neighbors.Residue!="HOH"]
+    aa_neighbors.drop_duplicates(inplace=True)
+    aa_neighbors['AA_Position'] = pd.to_numeric(aa_neighbors["AA_Position"])
+    RESULT = aa_neighbors.sort_values('AA_Position')  
+    return RESULT
+
 
 app = FastAPI()
 templates = Jinja2Templates(directory='static/')
@@ -285,5 +426,51 @@ async def post_chi(request: Request, pdb_id: str = Form(...)):
     getChi1Chi2plot(pdb_id, result)
     result = result.to_html()
     return templates.TemplateResponse("chi.html", context={
+        'request': request, 'result': result, 'pdb_id': pdb_id
+    })
+
+@app.get("/bs")
+def post_chi(request: Request):
+    result = "Enter PDB ID"
+    return templates.TemplateResponse("bs.html", context={
+        'request': request, 'result': result
+    })
+
+@app.post("/bs")
+async def post_chi(request: Request, pdb_id: str = Form(...)):
+    result = get_backbone_sidechain(pdb_id)
+    result = result.to_html()
+    return templates.TemplateResponse("bs.html", context={
+        'request': request, 'result': result, 'pdb_id': pdb_id
+    })
+
+@app.get("/metal")
+def post_chi(request: Request):
+    result = "Enter PDB ID"
+    return templates.TemplateResponse("metal.html", context={
+        'request': request, 'result': result
+    })
+
+@app.post("/metal")
+async def post_chi(request: Request, pdb_id: str = Form(...), ligand: str = Form(...), neighbor: float = Form(...)):
+    result = ligand_neighbors_distances(pdb_id, neighbor)
+    result = result.to_html()
+    return templates.TemplateResponse("metal.html", context={
+        'request': request, 'result': result, 'pdb_id': pdb_id
+    })
+
+
+@app.get("/aa")
+def post_chi(request: Request):
+    result = "Enter PDB ID"
+    return templates.TemplateResponse("aa.html", context={
+        'request': request, 'result': result
+    })
+
+@app.post("/aa")
+async def post_chi(request: Request, pdb_id: str = Form(...), neighbor: float = Form(...)):
+    result = aa_neighbors_distances(pdb_id, neighbor)
+    result = result.to_html()
+    return templates.TemplateResponse("aa.html", context={
         'request': request, 'result': result, 'pdb_id': pdb_id
     })
